b2d4ac17398a73789c38485be0e4cc684d37f8f8
Han <hanhaify@gmail.com>
2012-12-21 23:01:07 +0800

    [Han] update each activities in the AndroidMainfest.xml with fullname (package + basename)

--- lib/package_converter.rb
+++ lib/package_converter.rb
@@ -5,9 +5,25 @@ class PackageConverter
 
   def self.convert (new_package_name)
     raise "New package name should not be null or empty" if new_package_name.nil? || new_package_name.empty?
-    raise "Cannot find the android manifest file" if File.exist? "./" + ANDROID_MANIFEST
+    raise "Cannot find the android manifest file" unless File.exist? ANDROID_MANIFEST
 
-    xml = File.read ANDROID_MANIFEST
-    p xml
+    xml = REXML::Document.new(File.read ANDROID_MANIFEST)
+    manifest = xml.root
+    original_package = manifest.attributes["package"]
+
+    manifest.elements.each("application/activity") do |element|
+      p element
+      activity_name = element.attributes["android:name"]
+      element.attributes["android:name"] = activity_full_name(original_package, activity_name)
+      p element.attributes["android:name"]
+    end
+
+    File.open(ANDROID_MANIFEST, "w") do |data|
+      data << xml
+    end
+  end
+
+  def self.activity_full_name (package, basename)
+    package + basename if basename.to_s.start_with? "."
   end
 end

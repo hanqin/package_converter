5542e10934f3b62916dbbc7352156c10888085a0
Han <hanhaify@gmail.com>
2012-12-21 19:52:02 +0800

    [Han] add bundler to manage gems, rake to help build package_converter

--- lib/package_converter.rb
+++ lib/package_converter.rb
@@ -1,5 +1,13 @@
+require 'rexml/document'
+
 class PackageConverter
+  ANDROID_MANIFEST = "AndroidManifest.xml"
+
   def self.convert (new_package_name)
-    puts new_package_name
+    raise "New package name should not be null or empty" if new_package_name.nil? || new_package_name.empty?
+    raise "Cannot find the android manifest file" if File.exist? "./" + ANDROID_MANIFEST
+
+    xml = File.read ANDROID_MANIFEST
+    p xml
   end
 end

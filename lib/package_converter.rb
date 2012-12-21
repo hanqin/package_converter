6348e9d5a3e4c909b34aff790be6c8fd8aa032bf
Han <hanhaify@gmail.com>
2012-12-21 23:53:53 +0800

    [Han] update all src code with new R.java

--- lib/package_converter.rb
+++ lib/package_converter.rb
@@ -11,11 +11,26 @@ class PackageConverter
     manifest = xml.root
     original_package = manifest.attributes["package"]
 
+    manifest.attributes["package"] = new_package_name
+
     manifest.elements.each("application/activity") do |element|
       p element
       activity_name = element.attributes["android:name"]
       element.attributes["android:name"] = activity_full_name(original_package, activity_name)
       p element.attributes["android:name"]
+    end
+
+    Dir.glob ("src/**/*.java") do |file|
+      text = File.read(file)
+      text.gsub!("import #{original_package}.R;", "import #{new_package_name}.R;")
+      File.open(file, 'w') { |f| f.write(text) }
+    end
+
+    Dir.glob ("src/#{original_package.gsub(".", "/")}/*.java") do |file|
+      text = File.read(file)
+      next if text.include? "import #{new_package_name}.R;"
+      text.gsub!("package #{original_package};", "package #{original_package};\r\nimport #{new_package_name}.R;")
+      File.open(file, 'w') { |f| f.write(text) }
     end
 
     File.open(ANDROID_MANIFEST, "w") do |data|

3bd0200ad24bdbdded7fdf360e0507bf0a37f372
Han <hanhaify@gmail.com>
2012-12-22 20:40:15 +0800

    [Han] create a dir and copy projects to the dir and convert

--- lib/package_converter.rb
+++ lib/package_converter.rb
@@ -1,13 +1,22 @@
-require 'rexml/document'
+require "rexml/document"
+require "fileutils"
 
 class PackageConverter
   ANDROID_MANIFEST = "AndroidManifest.xml"
 
-  def self.convert (new_package_name, new_app_name)
+  def self.convert (new_package_name, new_app_name, src_dir, dest_dir)
     raise "New package name should not be null or empty" if new_package_name.nil? || new_package_name.empty?
-    raise "Cannot find the android manifest file" unless File.exist? ANDROID_MANIFEST
+    raise "Cannot find the android manifest file" unless File.exist? "#{src_dir}/#{ANDROID_MANIFEST}"
 
-    xml = REXML::Document.new(File.read ANDROID_MANIFEST)
+    copy_files(src_dir, dest_dir)
+
+    do_convert(new_app_name, new_package_name, dest_dir)
+  end
+
+  def self.do_convert(new_app_name, new_package_name, dest_dir)
+    target_manifest = "#{dest_dir}/#{ANDROID_MANIFEST}"
+
+    xml = REXML::Document.new(File.read target_manifest)
     manifest = xml.root
     original_package = manifest.attributes["package"]
 
@@ -17,21 +26,26 @@ class PackageConverter
 
     update_activity_name(manifest, original_package)
 
-    update_src_to_use_new_r(new_package_name, original_package)
+    update_src_to_use_new_r(new_package_name, original_package, dest_dir)
 
-    File.open(ANDROID_MANIFEST, "w") do |data|
+    File.open(target_manifest, "w") do |data|
       data << xml
     end
   end
 
-  def self.update_src_to_use_new_r(new_package_name, original_package)
-    Dir.glob ("src/**/*.java") do |file|
+  def self.copy_files(src_dir, dest_dir)
+    FileUtils.rm_rf dest_dir
+    FileUtils.cp_r src_dir, dest_dir
+  end
+
+  def self.update_src_to_use_new_r(new_package_name, original_package, dest_dir)
+    Dir.glob ("#{dest_dir}/src/**/*.java") do |file|
       text = File.read(file)
       text.gsub!("import #{original_package}.R;", "import #{new_package_name}.R;")
       File.open(file, 'w') { |f| f.write(text) }
     end
 
-    Dir.glob ("src/#{original_package.gsub(".", "/")}/*.java") do |file|
+    Dir.glob ("#{dest_dir}/src/#{original_package.gsub(".", "/")}/*.java") do |file|
       text = File.read(file)
       next if text.include? "import #{new_package_name}.R;"
       text.gsub!("package #{original_package};", "package #{original_package};\r\nimport #{new_package_name}.R;")
